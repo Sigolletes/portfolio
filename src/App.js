584eb954ebd8ff70f4bc9ab9b3824b314ec4dbeb
Sigolletes <webdevjudit@gmail.com>
2023-03-05 17:37:34 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('React running on GitHub pages');
   return (
     <div className="App">
       <header className="App-header">
