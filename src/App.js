ad3e5b99eb0afc17ea0436bc8a50d9ab9410c1d6
Sigolletes <webdevjudit@gmail.com>
2023-03-06 11:34:09 +0100

    Change App function to App.js

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Roadmap from "./pages/Roadmap";
+import Blog from "./pages/Blog";
+import NoPage from "./pages/NoPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="portfolio" element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="roadmap" element={<Roadmap />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
