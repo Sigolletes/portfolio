712578292469b83d4a6c7c0951b6cb2f6f05f7a1
Sigolletes <webdevjudit@gmail.com>
2023-03-08 21:01:30 +0100

    Add Contact info

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,33 @@
-import { ReactComponent as Email } from "../images/icons/email.svg"
+import { ReactComponent as Email } from "../images/icons/email.svg";
+import { ReactComponent as GitHub } from "../images/icons/github.svg";
+import { ReactComponent as Linkedin } from "../images/icons/linkedin.svg";
 
 const Contact = () => {
 	return (
-		<div>
-			<div className="contact">		
-				<a href = "mailto: webdevjudit@gmail.com" target="_blank" rel="noreferrer" ><Email /></a>
-				<a href = "mailto: webdevjudit@gmail.com" target="_blank" rel="noreferrer" >webdevjudit@gmail.com</a>
+		<div className="contact">	
+			<div>
+				<a href="https://github.com/Sigolletes" target="_blank" rel="noreferrer" >
+					<GitHub />
+				</a>
+				<a href="https://github.com/Sigolletes" target="_blank" rel="noreferrer" >
+					GitHub
+				</a>
+			</div>
+			<div>
+				<a href = "mailto: webdevjudit@gmail.com" target="_blank" rel="noreferrer" >
+					<Email />					
+				</a>
+				<a href = "mailto: webdevjudit@gmail.com" target="_blank" rel="noreferrer" >
+					webdevjudit@gmail.com
+				</a>
+			</div>	
+			<div>
+				<a href="#" target="_blank" rel="noreferrer" >
+					<Linkedin />
+				</a>
+				<a href="https://github.com/Sigolletes" target="_blank" rel="noreferrer" >
+					Linkedin
+				</a>
 			</div>
 		</div>
 	)
